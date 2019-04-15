48dfdef34558b01a7ce253cf0dd4a7bf47fce450
Jaqueline Trevizan <trevizan.jaqueline@gmail.com>
2019-04-15 11:39:35 -0300

    Commit exercício 3

--- exercicios/exercicio3_Jaqueline.py
+++ exercicios/exercicio3_Jaqueline.py
@@ -1,9 +1,8 @@
 # Leia uma frase e exiba quantas vogais aparecem na frase.
 
-frase = str(input('Digite uma frase: ')).upper()
+frase = str(input('Digite uma frase: ')).lower()
 contador = 0
-for f in frase:
-    for letra in f:
-        if letra.lower() in 'aeiou':
-            contador += 1
+for letra in frase:
+    if letra.lower() in 'aeiou':
+        contador += 1
 print('A frase digitada contém {} vogais.'.format(contador))
