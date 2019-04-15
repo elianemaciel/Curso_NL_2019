bacb52216ff866a043316ab66c080fd63e88a825
NL Informática <treinamento.nl@intranetnl.com.br>
2019-04-15 15:23:08 -0300

    exercicio5 do gabriel sgorla

--- exercicios/exercicio05_gabriel_sgorla.py
+++ exercicios/exercicio05_gabriel_sgorla.py
@@ -2,7 +2,7 @@ n1 = int(input(print("Digite o primeiro valor: ")))
 n2 = int(input(print("Digite o primeiro valor: ")))
 int maior
 
-if n1 > n2:
+if n1 >= n2:
     maior = n1
 else :
     maior = n2
