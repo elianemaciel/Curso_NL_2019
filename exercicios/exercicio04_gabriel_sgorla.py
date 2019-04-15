fc1cd9c992c64dce0a8fc8e0f59aa0c87405672a
NL Informática <treinamento.nl@intranetnl.com.br>
2019-04-15 15:11:33 -0300

    Exercicio4 do gabriel sgorla

---
+++ exercicios/exercicio04_gabriel_sgorla.py
@@ -0,0 +1,9 @@
+lista = [-30,-30,-20,-10,10,20,30,40,50]
+maior = max(lista)
+print("O maior da lista é: ", maior)
+soma = sum(lista)
+print("A soma dos elementos é: ", soma)
+num = lista.count(lista[0])
+print("O número de ocorrências do primeiro elemento é: ", num)
+media = soma/len(lista)
+print("A média dos elementos é: ", media)
