9f91c55759e55ca7bc6f67bda38e59717b207694
GabrielKuwerRech <Gabrielkr.gr@gmail.com>
2019-04-17 11:41:10 -0300

    Exercicio 20 do Gabriel Rech

---
+++ exercicios/Exercicio20_GabrielRech.py
@@ -0,0 +1,32 @@
+
+'''exercicio feito por David, Gabriel Rech e Gabriel Sgorla'''
+
+import random
+
+lista1 = random.sample(range(0,20),5)
+
+lista2 = random.sample(range(0,20),10)
+
+lista3 = []
+
+
+
+print(lista1)
+
+print("----------------------------------------------------------")
+
+print(lista2)
+
+print("----------------------------------------------------------")
+
+'''for i in lista1:
+
+    if i in lista2:
+
+        lista3.append(i)
+
+print(lista3)'''
+
+
+
+print([i for i in lista1 if i in lista2])
